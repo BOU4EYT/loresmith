7af60240e6d911942699babe37570460f68de369
BOU4EYT <67209110+BOU4EYT@users.noreply.github.com>
2026-03-12 14:37:52 -0400

    Make createClient function asynchronous
    Change createClient to be an async function to await cookies.

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,8 +1,8 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export function createClient() {
-  const jar = cookies()
+export async function createClient() {
+  const jar = await cookies()
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
